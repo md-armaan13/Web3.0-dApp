7af27172858e206c876e5cc7883f1ad90d7223ca
MD ARMAAN ANSARI <arman.kanpur4080@gmail.com>
2022-10-16 19:35:26 +0530

    adding react repo and deploying our smart contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -17,4 +17,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: "https://methodical-warmhearted-lambo.ethereum-goerli.discover.quiknode.pro/b75f5e0ed8dc69acfea4a504d25827b66046a94e/",
+      accounts: ["6fb6b08acf3a52027d7cde06c9c9c54085f738384a3a7e9d46d8ac1a81803dc4"]
+    },
+  },
 };
