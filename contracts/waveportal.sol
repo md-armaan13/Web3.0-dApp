18e1a52dc661e09da5f354fa3577e73260a17d09
MD ARMAAN ANSARI <arman.kanpur4080@gmail.com>
2022-10-16 17:49:57 +0530

    implementing wave function adding comment,startingup guide,some other person want to wave at us

--- contracts/waveportal.sol
+++ contracts/waveportal.sol
@@ -1,12 +1,28 @@
 // SPDX-License-Identifier: UNLICENSED
 
-pragma solidity ^0.8.17; 
+pragma solidity ^0.8.17; //This is the version of the Solidity compiler we want our contract to use
+
 // version of solidity
 
-import "hardhat/console.sol";
+import "hardhat/console.sol";//Some magic given to us by Hardhat to do some console logs in our contract
 
+
+//Once we initialize this contract for the first time, that constructor will run and print out that line
 contract WavePortal {
+//we also added a totalWaves variable that automatically is initialized to 0. 
+//But, this variable is special because it's called a "state variable" and it's cool because it's stored permanently in contract storage.
+    uint256 totalWaves;
+
     constructor() {
         console.log("Yo yo, I am a contract and I am smart");
+    }
+      function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);//msg.sender. This is the wallet address of the person who called the function
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,19 +1,45 @@
 const main = async () => {
-    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-    const waveContract = await waveContractFactory.deploy();
-    await waveContract.deployed();
-    console.log("Contract deployed to:", waveContract.address);
-  };
-  
-  const runMain = async () => {
-    try {
-      await main();
-      process.exit(0); // exit Node process without error
-    } catch (error) {
-      console.log(error);
-      process.exit(1); // exit Node process while indicating 'Uncaught Fatal Exception' error
-    }
-    // Read more about Node exit ('process.exit(num)') status codes here: https://stackoverflow.com/a/47163396/7974948
-  };
-  
-  runMain();
+  const [owner, randomPerson] = await hre.ethers.getSigners()//In order to deploy something to the blockchain, we need to have a wallet address! Hardhat does this for us magically in the background, 
+  //but here I grabbed the wallet address of contract owner and I also grabbed a random wallet address and called it randomPerson. This will make more sense in a moment.
+
+  const waveContractFactory = await hre.ethers.getContractFactory('WavePortal') //This will actually compile our contract and generate the necessary
+  //files we need to work with our contract under the artifacts directory
+
+  //What's happening here is Hardhat will create a local Ethereum network for us, but just for this contract.
+  // Then, after the script completes it'll destroy that local network. So, every time you run the contract, it'll be a fresh blockchain
+  const waveContract = await waveContractFactory.deploy()
+    
+
+  //We'll wait until our contract is officially deployed to our local blockchain! Our constructor runs when we actually deploy.
+  await waveContract.deployed()
+
+
+  console.log('Contract deployed to:', waveContract.address)
+  console.log('Contract deployed by:', owner.address)// address of the person deploying our contract.
+
+  await waveContract.getTotalWaves()
+
+  const firstwaveTxn = await waveContract.wave()
+  await firstwaveTxn.wait()
+
+  await waveContract.getTotalWaves()
+
+  //WHEN YOU  SOME OTHER PERSON WANT TO WAVE AT YOU
+  const secondWaveTxn = await waveContract.connect(randomPerson).wave()
+  await secondWaveTxn.wait()
+
+  await waveContract.getTotalWaves()
+}
+
+const runMain = async () => {
+  try {
+    await main()
+    process.exit(0) // exit Node process without error
+  } catch (error) {
+    console.log(error)
+    process.exit(1) // exit Node process while indicating 'Uncaught Fatal Exception' error
+  }
+  // Read more about Node exit ('process.exit(num)') status codes here: https://stackoverflow.com/a/47163396/7974948
+}
+
+runMain()
